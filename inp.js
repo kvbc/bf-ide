b8f61147b461b21b5c199f29ca7f2ebbe2009893
kvbc <64214044+kvbc@users.noreply.github.com>
2020-07-30 09:39:53 +0200

    Update inp.js

--- inp.js
+++ inp.js
@@ -1,5 +1,5 @@
-function Interpreter(cells = 30000) {
-    this.stack = new Array(cells).fill(0);
+function Interpreter(cells) {
+    this.mem   = new Array(cells).fill(0);
     this.code  = editor.getValue().BF();
     this.mp    = 0; // memory pointer
     this.ip    = 0; // instruction pointer
@@ -11,7 +11,7 @@ function Interpreter(cells = 30000) {
 
 Interpreter.prototype.data = function() {
     return {
-        stack: this.stack,
+        mem: this.mem,
         code: this.code,
         ip: this.ip,
         mp: this.mp
@@ -25,11 +25,11 @@ Interpreter.prototype.eoc = function() {
 
 
 Interpreter.prototype.init = function() {
-    let stack = [];
+    let mem = [];
     for(let i = 0; i < this.code.length; i++)
-        if(this.code[i] == '[') stack.push(i);
+        if(this.code[i] == '[') mem.push(i);
         else if(this.code[i] == ']') {
-            let start = stack.pop();
+            let start = mem.pop();
             this.start[i] = start;
             this.end[start] = i;
         }
@@ -39,34 +39,34 @@ Interpreter.prototype.init = function() {
 Interpreter.prototype.step = function(by = 1) {
     switch(this.code[this.ip]) {
         case '>':
-            if(++this.mp >= this.stack.length)
+            if(++this.mp >= this.mem.length)
                 this.mp = 0;
             break;
         case '<':
             if(--this.mp < 0)
-                this.mp = this.stack.length - 1;
+                this.mp = this.mem.length - 1;
             break;
         case '+':
-            if(++this.stack[this.mp] > 255)
-                this.stack[this.mp] = 0;
+            if(++this.mem[this.mp] > 255)
+                this.mem[this.mp] = 0;
             break;
         case '-':
-            if(--this.stack[this.mp] < 0)
-                this.stack[this.mp] = 255;
+            if(--this.mem[this.mp] < 0)
+                this.mem[this.mp] = 255;
             break;
         case '.':
-            BF_INP.el.output.value += String.fromCharCode(this.stack[this.mp]);
+            BF_INP.el.output.value += String.fromCharCode(this.mem[this.mp]);
             break;
         case ',':
             if(this.inpi >= BF_INP.el.input.value.length) break;
-            this.stack[this.mp] = BF_INP.el.input.value.charCodeAt(this.inpi++);
+            this.mem[this.mp] = BF_INP.el.input.value.charCodeAt(this.inpi++);
             break;
         case '[':
-            if(this.stack[this.mp] == 0)
+            if(this.mem[this.mp] == 0)
                 this.ip = this.end[this.ip];
             break;
         case ']':
-            if(this.stack[this.mp] != 0)
+            if(this.mem[this.mp] != 0)
                 this.ip = this.start[this.ip];
             break;
     }
